c5d44bb38c885e44b4af321272133b1dd6c3a782
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-03-17 22:28:39 +0700

    update learning

--- javascript-dom/newLearning/learning5/script.mjs
+++ javascript-dom/newLearning/learning5/script.mjs
@@ -1,14 +1,132 @@
-import number from "./number.mjs"
+// import number from "./number.mjs"
+let prompt = document.getElementById('prompt');
+let promptAttr = prompt.getAttributeNode('value');
+let rawData = [];
+let total = null;
+let status = ""
 
-let prompt = document.getElementById('prompt')
-let promptAttr = prompt.getAttributeNode('value')
+let funcGetElement = (ids) => {
+    let result = [];
+    for (const id of ids) {
+        let element = document.getElementById(`${id}`)
+        result.push({element_docs: element, type:element.getAttribute('class') })
+    }
+    return result;
+}
 
-let funcOnClick = (elementDoc) => {
-    elementDoc.onclick = (event) => {
-        console.info(promptAttr.value)
+let mainFunction = function (elementDoc) {
+    let status = elementDoc.getAttribute('class')
+
+    if (status == 'data-number') {
+        funcNumber(elementDoc)
+    } else if (status == 'operation-operator') {
+        funcOperator(elementDoc)
     }
 }
 
-let arrayDocs = number.arrayDocument
+let funcNumber = (parameterNumber) => {
+    let valueNumber = parameterNumber.getAttribute('value')
 
-console.info(arrayDocs)
+    parameterNumber.onclick = () => {
+        if (parseInt(promptAttr.value) == 0 || status == "operator") {
+            promptAttr.value = valueNumber;
+            status = "input"
+        } else {
+            promptAttr.value = promptAttr.value + valueNumber; 
+            status = "input"
+        }
+    }
+}
+
+let funcOperator = (parameterOperator) => {
+    let getAttrVal = parameterOperator.getAttribute('value');
+
+    parameterOperator.onclick = () => {
+        if (getAttrVal == "+") {
+            (total == null) ? total = parseInt(promptAttr.value) : total += parseInt(promptAttr.value)
+            status = "operator"
+            setAttribute(getAttrVal)
+            promptAttr.value = total;
+        } else if (getAttrVal == "-") {
+            (total == null) ? total = parseInt(promptAttr.value) : total -= parseInt(promptAttr.value)
+            status = "operator"
+            setAttribute(getAttrVal)
+            promptAttr.value = total;
+        } else if (getAttrVal == "*") {
+            (total == null) ? total = parseInt(promptAttr.value) : total *= parseInt(promptAttr.value)
+            status = "operator"
+            setAttribute(getAttrVal)
+            promptAttr.value = total;
+        } else if (getAttrVal == "/") {
+            (total == null) ? total = parseInt(promptAttr.value) : total /= parseInt(promptAttr.value)
+            status = "operator"
+            setAttribute(getAttrVal)
+            promptAttr.value = total;
+        } else if (getAttrVal == 'CE') {
+            let inputOperator = document.getElementById("operator");
+            let operator = inputOperator.getAttributeNode("value");
+            operator.value = "";
+            promptAttr.value = 0;
+            total = 0;
+            status = "";
+        } else if (getAttrVal == '=') {
+            let value = promptAttr.value;
+            funcEqual(value)
+        }
+    }
+}
+
+let funcEqual = (value) => {
+    let inputOperator = document.getElementById("operator");
+    let operator = inputOperator.getAttributeNode("value");
+
+    switch (operator.value) {
+        case "+":
+            total += parseInt(value);
+            promptAttr.value = total;
+            operator.value = "=";
+            total = null;
+            status = "operator";
+            break;
+        case "-":
+            total -= parseInt(value);
+            promptAttr.value = total;
+            operator.value = "=";
+            total = null;
+            status = "operator";
+            break;
+        case "*":
+            total *= parseInt(value);
+            promptAttr.value = total;
+            operator.value = "=";
+            total = null;
+            status = "operator";
+            break;
+        case "/":
+            total /= parseInt(value);
+            promptAttr.value = total;
+            operator.value = "=";
+            total = null;
+            status = "operator";
+            break;
+        default:
+            break;
+    }
+}
+
+let setAttribute = (value) => {
+    let propmtOperator = document.getElementById('operator')
+    let attribute = document.createAttribute('value')
+
+    attribute.value = value
+
+    propmtOperator.setAttributeNode(attribute)
+}
+
+let dataButton = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "dot","0", "00", "delete", "divide", "times", "minus", "plus", "equal"];
+
+let arrayButton = funcGetElement(dataButton);
+
+for (const dataButton of arrayButton) {
+    mainFunction(dataButton.element_docs);
+}
